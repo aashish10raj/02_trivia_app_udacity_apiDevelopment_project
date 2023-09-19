44bea491af71fe81f2b4bb5817a7958fc68a08fa
Aashish Raj <aashish10raj@gmail.com>
2023-09-19 10:56:39 +0530

    all endpoints working

--- cd0037-API-Development-and-Documentation-project/frontend/src/components/QuestionView.js
+++ cd0037-API-Development-and-Documentation-project/frontend/src/components/QuestionView.js
@@ -22,7 +22,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `http://localhost:8000/questions?page=${this.state.page}`, //TODO: update request URL
       type: 'GET',
       success: (result) => {
         this.setState({
